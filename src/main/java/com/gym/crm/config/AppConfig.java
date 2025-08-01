a5e421dfeb4d83417b1c0d43f24346e984c2e3db
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-08-01 11:15:56 +0300

    GCA-124 Migrate to Spring Boot - Update configuration

--- src/main/java/com/gym/crm/config/AppConfig.java
+++ src/main/java/com/gym/crm/config/AppConfig.java
@@ -5,51 +5,25 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.security.SecureRandom;
 import java.util.List;
-import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.gym.crm", "org.springdoc"})
 public class AppConfig implements WebMvcConfigurer {
     @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(ConfigurableEnvironment env) {
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-
-        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-        yaml.setResources(new ClassPathResource("application.yml"));
-
-        Properties properties = yaml.getObject();
-        PropertySource<?> propertySource = new PropertiesPropertySource("yamlProps", properties);
-        env.getPropertySources().addFirst(propertySource);
-
-        configurer.setProperties(properties);
-
-        return configurer;
-    }
-
-    @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
-
         return mapper;
     }
 
@@ -61,26 +35,18 @@ public class AppConfig implements WebMvcConfigurer {
                         .version("1.0")
                         .description("Comprehensive API for Gym Customer Relationship Management System"))
                 .servers(List.of(
-                        new Server().url("http://localhost:8080/gym-crm-core").description("Default Server")
+                        new Server().url("http://localhost:8080").description("Default Server")
                 ));
     }
 
     @Bean
-    public MethodValidationPostProcessor methodValidationPostProcessor() {
-        return new MethodValidationPostProcessor();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
-        return new MappingJackson2HttpMessageConverter(objectMapper);
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+    public SecureRandom secureRandom() {
+        return new SecureRandom();
     }
 
     @Override
